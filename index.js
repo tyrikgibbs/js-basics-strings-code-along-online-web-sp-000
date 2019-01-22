fd9ca4044aeaabaf45acfd37daeeee618a3d65c9
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-01-22 00:29:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Gibson';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
